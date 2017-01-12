8270efb4e331c12feee6d6121f5f53e92097212a
Steve <stephentwillis@gmail.com>
2017-01-12 21:55:40 +0000

    2017 updates
    2017 fee guide, fee alteration on family page, Kerin to

---
+++ FerndaleHouse/images/gallery/index_gallery.php
@@ -0,0 +1,58 @@
+	
+		<div class="content">
+			<div id="myGallery">			
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/1.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/15.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/16.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/13.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/14.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://www.ferndaledental.co.uk/assets/images/gallery/4.png" class="full" alt="" />
+				</div>
+									
+				<!--<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/5.png" class="full" alt="" />
+				</div>-->
+				
+				<!--<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/6.png" class="full" alt="" />
+				</div>-->
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/7.png" class="full" alt="" />
+				</div>
+				
+				<!--<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/8.png" class="full" alt="" />
+				</div>-->
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/9.png" class="full" alt="" />
+				</div>
+				
+				<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/10.png" class="full" alt="" />
+				</div>
+								
+				<!--<div class="GalleryItem">
+					<img src="http://d3n3s2u09g36ri.cloudfront.net/assets/images/gallery/12.png" class="full" alt="" />
+				</div>-->
+				
+			</div>
+		</div>
