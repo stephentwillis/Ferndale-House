a8663ced4b57a2405d00a9a843137deb13291110
Steve <stephentwillis@gmail.com>
2017-02-04 19:53:15 +0000

    sub-directories

---
+++ FerndaleHouse/bootstrap/our-services/about-us/welcome.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<title>Welcome | Ferndale House Dental Practice | The Patient People | Dentist in Chesterfield</title>
+
+	<?php include("assets/includes/master-css.php"); ?>
+</head>
+
+<body>
+	<div class="wrapper">
+		<!--=== Header v4 ===-->
+		<?php include("assets/includes/header.php"); ?>
+		<!--=== End Header v4 ===-->
+
+		<!--=== Content Part ===-->
+		<div class="container content">
+			<div class="row magazine-page">
+				<!-- Begin Content -->
+                <div class="col-md-12">
+                    <h2>Links</h2>
+                </div>
+
+				<div class="col-md-8">
+
+					<!-- Magazine News -->
+					<div class="magazine-news">
+						<div class="row">
+
+							
+
+
+						</div>
+					</div>
+					<!-- End Magazine News -->					
+				</div>
+				<!-- End Content -->
+
+				<!-- Begin Sidebar -->
+				<div class="col-md-4">
+                    <?php include("assets/includes/form.php"); ?>
+                </div>
+				<!-- End Sidebar -->
+			</div>
+		</div><!--/container-->
+		<!-- End Content Part -->
+
+		<!--=== Owl Clients v1 ===-->
+		<?php include("assets/includes/logos.php"); ?>
+		<!--=== End Owl Clients v1 ===-->
+
+		<!--=== Footer Version 1 ===-->
+		<?php include("assets/includes/footer.php"); ?>
+		<!--=== End Footer Version 1 ===-->
+	</div><!--/wrapper-->
+
+	<?php include("assets/includes/master-js.php"); ?>
+
+	<?php include_once("assets/includes/analytics.php") ?>
+</body>
+</html>
