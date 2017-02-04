e5acac56661d819a27d0898cea0a676e8ed63720
Steve <stephentwillis@gmail.com>
2017-02-04 18:45:17 +0000

    Form validation, error styles etc

--- FerndaleHouse/bootstrap/assets/includes/form.php
+++ FerndaleHouse/bootstrap/assets/includes/form.php
@@ -2,27 +2,28 @@
     <h2 class="text-center">Get in touch</h2>
     <form id="enquiry-form" name="enquiryform" action="assets/php/enquiry.php" method="post">                                               
         <div class="form-group">
-            <label for="name">Full Name:</label>            
+            <label for="name">Full Name:</label><span class="required"><b>*</b> Required</span>
             <input id="name" type="text" name="name" class="form-control" tabindex="1" />
         </div>                            
         <div class="form-group">
-            <label for="address">Address and postcode:</label>         
+            <label for="address">Address and postcode:</label><span class="required"><b>*</b> Required</span>
             <textarea name="address" id="address" class="form-control" tabindex="2" rows="5"></textarea>
         </div>                            
         <div class="form-group">
-            <label for="telephone">Telephone:</label>     
+            <label for="telephone">Telephone:</label><span class="required"><b>*</b> Required</span>
             <input type="telephone" name="telephone" class="form-control" id="telephone" tabindex="3" />
         </div>                            
         <div class="form-group">
-            <label for="email">Email address:</label>          
+            <label for="email">Email address:</label><span class="required"><b>*</b> Required</span>
             <input id="email" type="text" name="email" class="form-control" tabindex="4" />
         </div>                            
         <div class="form-group">
-            <label for="enquiry">How can we help?</label>                            
+            <label for="enquiry">How can we help?</label><span class="required"><b>*</b> Required</span>
             <textarea id="enquiry" name="enquiry" class="form-control" tabindex="5" rows="10"></textarea>
         </div>                            
         <div class="form-group">
-            <button class="btn btn-default" type="submit" tabindex="6">Submit</button>
+            <p id="form-error" class="bg-danger text-center">Please complete the required fields</p>
+            <button id="submit" class="btn btn-default" type="submit" tabindex="6">Submit</button>
         </div>                    
     </form>
 </div>
--- FerndaleHouse/bootstrap/assets/js/custom.js
+++ FerndaleHouse/bootstrap/assets/js/custom.js
@@ -1,1 +1,36 @@
-/* Write here your custom javascript codes */
+jQuery(document).ready(function() {
+
+	jQuery(".form-control").blur(function() {
+		var $this = jQuery(this);
+
+		if ($this.val() == "" || $.trim($this.val()) == "") {
+			$this.addClass("error");
+		} else {
+			$this.removeClass("error");
+		}
+	});
+
+	jQuery("#submit").click(function(e) {
+		e.preventDefault;
+
+		jQuery(".form-control").trigger("blur");
+
+		if (!jQuery(".form-control").hasClass("error")) {
+
+			try {
+				jQuery("#form-error").hide();
+
+				jQuery("#enquiry-form").submit();
+
+				return true;			
+			} catch(e) {
+				console.log(e);
+			}
+		} else {
+			jQuery("#form-error").show();
+		}
+
+		return false;
+	});
+
+});
